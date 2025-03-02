3c41eee414d9b9c1bfac7dde413680b2cf0c6a61
luis <luis@casta.dev>
2025-03-01 18:30:46 -0600

    fourth part of my bank app, jwt

--- api.go
+++ api.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
+	"time"
 )
 
 type ApiServer struct {
@@ -26,8 +29,11 @@ func (s *ApiServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount)).Methods("GET")
-	router.HandleFunc("/account/{id}", makeHttpHandleFunc(s.handleGetAccountById)).Methods("GET")
+
+	// needs to be auth
+	router.HandleFunc("/account/{id}", withJwtAuth(makeHttpHandleFunc(s.handleGetAccountById), s.store)).Methods("GET")
 	router.HandleFunc("/account/{id}", makeHttpHandleFunc(s.handleDeleteAccount)).Methods("DELETE")
+
 	router.HandleFunc("/transfer", makeHttpHandleFunc(s.handleTransfer)).Methods("POST")
 
 	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
@@ -91,13 +97,17 @@ func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	defer r.Body.Close()
 
 	account := NewAccount(accRequest.FirstName, accRequest.LastName, accRequest.Balance)
-
 	if err := s.store.CreateAccount(account); err != nil {
 		fmt.Println("Error creating account")
 		return err
 	}
 
-	fmt.Printf("Welcome %s, with balance=%d", accRequest.FirstName, accRequest.Balance)
+	tokenString, err := createJwt(account)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Welcome %s, with balance=%d, token=%s", accRequest.FirstName, accRequest.Balance, tokenString)
 
 	return WriteJson(w, http.StatusOK, account)
 
@@ -138,6 +148,88 @@ func WriteJson(w http.ResponseWriter, status int, v any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
+}
+
+// interesting, notice the decoration of this.
+// now it can be modified before calling the actual function in the router
+func withJwtAuth(handlerFunc http.HandlerFunc, storage Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Calling JWT auth middleware")
+
+		tokenString := r.Header.Get("x-jwt-token")
+		token, err := validateJwt(tokenString)
+		if err != nil {
+			fmt.Println("Unable to validate token")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "permission denied"})
+			return
+		}
+
+		if !token.Valid {
+			fmt.Println("Invalid Token")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "permission denied"})
+			return
+		}
+
+		paramId, err := getParameter(r, "id")
+		if err != nil {
+			fmt.Println("No parameter Id")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "permission denied"})
+			return
+		}
+		accountId, err := strconv.Atoi(paramId)
+		if err != nil {
+			fmt.Println("Invalid Id")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "permission denied"})
+			return
+		}
+
+		account, err := storage.GetAccountById(accountId)
+		if err != nil {
+			fmt.Println("Could not retrieve for id")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "permission denied"})
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		claimAccountNumber, ok := claims["accountNumber"].(float64)
+		if !ok {
+			fmt.Println("Invalid claim type for accountNumber")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "invalid token"})
+			return
+		}
+
+		if account.Number != int64(claimAccountNumber) {
+			fmt.Println("Insufficient Permissions")
+			WriteJson(w, http.StatusForbidden, ApiError{Error: "permission denied"})
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func validateJwt(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(secret), nil
+	})
+}
+
+func createJwt(account *Account) (string, error) {
+	// Create the Claims
+	claims := &jwt.MapClaims{
+		"expiresAt":     jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		"accountNumber": account.Number,
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
 }
 
 // my functions are of this type by virtue of the signature
